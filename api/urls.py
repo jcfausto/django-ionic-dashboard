b51924e7a8cfd937cbea54a9b3fc63035622c93b
Julio Cesar Fausto <jcfausto@gmail.com>
2016-01-22 15:25:42 +0000

    Organizations list endpoint working at /api/organizations/

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from api.views import PingView
+from api.views import PingView, OrganizationView
 
 urlpatterns = patterns('',
 	url(r'/ping/', PingView.as_view(), name='ping'),
+	url(r'/organizations/', OrganizationView.as_view(), name='organizations')
 )
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .mixins import JSONResponseMixin
 from rest_framework.views import APIView
+from api.models import Organization
+from api.serializers import OrganizationSerializer
 
 class PingView(APIView):
 
@@ -11,3 +13,14 @@ class PingView(APIView):
 		data = {'ping': 'pong'}
 
 		return JSONResponseMixin(data)
+
+
+class OrganizationView(APIView):
+
+	def get(self, request):
+		"""
+		List all organizations registered in the system
+		"""
+		organizations = Organization.objects.all()
+		serialized_data = OrganizationSerializer(organizations, many=True)
+		return JSONResponseMixin(serialized_data.data)
