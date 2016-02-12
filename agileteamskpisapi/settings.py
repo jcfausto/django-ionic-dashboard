a47e99446d9b62b04bd614c2553880cbf75c5f07
Julio Cesar Fausto <jcfausto@gmail.com>
2016-02-12 13:21:21 +0000

    Added static processor in TEMPLATE settings

--- agileteamskpisapi/settings.py
+++ agileteamskpisapi/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.core.context_processors.static',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
