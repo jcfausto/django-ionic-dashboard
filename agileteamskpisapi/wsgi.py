8c4d3a23dde86da0b4a0ce75de0e5d3a8023bb69
Julio Cesar Fausto <jcfausto@gmail.com>
2016-02-12 13:34:04 +0000

    cling setup

--- agileteamskpisapi/wsgi.py
+++ agileteamskpisapi/wsgi.py
@@ -7,10 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agileteamskpisapi.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
